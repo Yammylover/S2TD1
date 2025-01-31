628e887c7db6243114ccca3682dc76389c200115
Yammylover <gregoire.marie1@etu.cyu.fr>
2025-01-31 11:44:47 +0100

    Add files via upload

--- exo1.3.c
+++ exo1.3.c
@@ -2,11 +2,12 @@
 #include <stdlib.h>
 #include <time.h>
 #include <math.h>
+#include <unistd.h>
 
 typedef struct{
 	float atk;
 	float def;
-	float dodge;
+	float agl;
 	float hp;
 	char name[32];
 } Ninja;
@@ -15,7 +16,7 @@ Ninja createFighter(){
 	Ninja n;
 	n.atk=rand()%6+5;
 	n.def=rand()%3+2;
-	n.dodge=(rand()%2+1)/10;
+	n.agl=(rand()%11+10)/100.0;
 	n.hp=rand()%21+80;
 	printf("\nRentrez le nom du combattant: \n");
 	scanf("%s",n.name);
@@ -23,39 +24,54 @@ Ninja createFighter(){
 }
 
 void displayfighter(Ninja n){
-	printf("\n%s\n-ATK:%f\n-DEF:%f\n-Dodge:%f\n-HP:%f\n",n.name,n.atk,n.def,n.dodge,n.hp);
+	printf("\n%s\n-ATK:%f\n-DEF:%f\n-Agilité:%f\n-HP:%f\n",n.name,n.atk,n.def,n.agl,n.hp);
 
 }
 	
 void FIGHT(Ninja a, Ninja b){
 	float dodge;
 	float damage=0;
-	
+	float critical;
 	while(a.hp>0 && b.hp>0){
-		dodge=rand()%101/10;
-		if (dodge>b.dodge){
-			damage=a.atk-b.def;
-			if(damage<0){
-				damage=0;
+		dodge=(rand()%101)/100.0;
+		critical=(rand()%101)/100.0;
+		if (dodge>b.agl){
+			if(a.agl>=critical){
+				damage=a.atk*2;
+				b.hp-=damage;
+				printf("%s takes a strong hit from %s for %f damages!\n",b.name,a.name,damage);
+			} else{			
+				damage=a.atk-b.def;
+				if(damage<0){
+					damage=0;
+				}
+				b.hp-=damage;
+				printf("%s takes %f damages from %s.\n",b.name,damage,a.name);
 			}
-			b.hp-=damage;
-			printf("%s takes %f damages from %s.\n",b.name,damage,a.name);
 		}
 		else {
 			printf("%s dodges attack from %s!\n",b.name,a.name);
 		}
-		dodge=rand()%101/10;
-		if (dodge>a.dodge){
-			damage=b.atk-a.def;
-			if(damage<0){
-				damage=0;
+		dodge=(rand()%101)/100.0;
+		critical=(rand()%101)/100.0;
+		if (dodge>a.agl){
+			if(b.agl>=critical){
+				damage=b.atk*2;
+				a.hp-=damage;
+				printf("%s takes a strong hit from %s for %f damages!\n",a.name,b.name,damage);
+			} else{			
+				damage=a.atk-b.def;
+				if(damage<0){
+					damage=0;
+				}
+				a.hp-=damage;
+				printf("%s takes %f damages from %s.\n",a.name,damage,b.name);
 			}
-			a.hp-=damage;
-			printf("%s takes %f damages from %s.\n",a.name,damage,b.name);
 		}
 		else {
 			printf("%s dodges attack from %s!\n",a.name,b.name);
 		}
+		sleep(1);
 	}
 //check result
 	if (a.hp<=0 && b.hp<=0){
@@ -75,6 +91,7 @@ int main(){
 	displayfighter(Nikoli);
 	Carey=createFighter(Carey);
 	displayfighter(Carey);
+	sleep(1);
 	FIGHT(Nikoli, Carey);
 return 0;
 }
